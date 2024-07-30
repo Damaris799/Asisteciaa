86099818930872483e5953dbc21cabfacc2b167e
Damaris5416 <damarisnayeliperezjuarez01@gmail.com>
2024-07-29 16:44:24 -1200

    jjjjj

---
+++ src/main/java/com/ESFE/Asistenciaa/AsistenciaaApplication.java
@@ -0,0 +1,13 @@
+package com.ESFE.Asistenciaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenciaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ESFE/Asistenciaa/AsistenciaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ESFE.Asistenciaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
